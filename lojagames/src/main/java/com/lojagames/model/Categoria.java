520992e0f439d1a36e971770f5b9fc42867b27d7
IgorTecnologia <iguigu2019@gmail.com>
2023-12-14 14:06:02 -0300

    Model

---
+++ lojagames/src/main/java/com/lojagames/model/Categoria.java
@@ -0,0 +1,43 @@
+package com.lojagames.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column (length = 100)
+	@NotBlank(message = "O atributo genero é obrigatório e não aceita espaços vazios!")
+	private String genero;
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	
+	}
+	
+	
