d7ec8d54a94df2f0e173d47cd0498faa31dc7056
IgorTecnologia <iguigu2019@gmail.com>
2023-12-14 16:05:37 -0300

    Model

--- lojagames/src/main/java/com/lojagames/model/Categoria.java
+++ lojagames/src/main/java/com/lojagames/model/Categoria.java
@@ -1,10 +1,17 @@
 package com.lojagames.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
@@ -20,6 +27,9 @@ public class Categoria {
 	@NotBlank(message = "O atributo genero é obrigatório e não aceita espaços vazios!")
 	private String genero;
 	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -35,6 +45,14 @@ public class Categoria {
 
 	public void setGenero(String genero) {
 		this.genero = genero;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 
 	
---
+++ lojagames/src/main/java/com/lojagames/model/Produto.java
@@ -0,0 +1,80 @@
+package com.lojagames.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 50)
+	@NotNull(message = "O campo nome é obrigatório!")
+	private String titulo;
+	
+	@Column(length = 50)
+	@NotNull(message = "O campo console é obrigatório!")
+	private String plataforma;
+	
+	@Column(length = 10)
+	@NotNull(message = "O campo classificação é obrigatório!")
+	private String classificacao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public String getClassificacao() {
+		return classificacao;
+	}
+
+	public void setClassificacao(String classificacao) {
+		this.classificacao = classificacao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
